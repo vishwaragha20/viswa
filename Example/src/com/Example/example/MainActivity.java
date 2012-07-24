811710bb2323220c3705523775892978a458704f
vishwaragha20 <vishwaragha20@gmail.com>
2012-07-24 17:20:44 +0530

    change...

--- Example/src/com/Example/example/MainActivity.java
+++ Example/src/com/Example/example/MainActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
+	Button b1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button1);
     }
 
     @Override
